b9e5c55db46034253f35d57fecc553cd9ce56312
fdl-11 <97675420+fdl-11@users.noreply.github.com>
2022-01-13 19:20:50 +0700

    Add files via upload

---
+++ pegawai.php
@@ -0,0 +1,235 @@
+<?php
+require 'function.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Record Transaksi</title>
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php">Database Rental Mobil</a>
+        <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                            <div>
+                                <h3 class="sb-sidenav-menu-heading" style="margin-left: 60px;">Welcome</h3>
+                                <img src="assets/img/settings.png" alt="" style="margin-left: 70px;">
+                            </div>
+
+                            <div class="sb-sidenav-menu-heading">Menu</div>
+                        <a class="nav-link" href="index.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-cut"></i></div>
+                            Cabang
+                        </a>
+                        <a class="nav-link" href="extra.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-plus"></i></div>
+                             Pelayanan Extra
+                        </a>
+                        <a class="nav-link" href="transaksi.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-credit-card"></i></div>
+                            Transaksi
+                        </a>
+                        <a class="nav-link" href="pegawai.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-user-tie"></i></div>
+                            Pegawai
+                        </a>
+                        <a class="nav-link" href="log.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-history"></i></div>
+                            Log
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid">
+                    <h1 class="mt-4">Data Pegawai</h1>
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pgwmodal">
+                                Tambah Pegawai
+                            </button>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>id_pegawai</th>
+                                            <th>nama_pegawai</th>
+                                            <th>alamat</th>
+                                            <th>cabang</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $tampilpgw = mysqli_query($conn, "select * from pegawai left join cabang on pegawai.id_cabang=cabang.id_cabang");
+                                        while ($data = mysqli_fetch_array($tampilpgw)) {
+                                            $id_pegawai = $data['id_pegawai'];
+                                            $nama_pegawai = $data['nama_pegawai'];
+                                            $alamat_pgw = $data['alamat_pgw'];
+                                            $nama_cabang = $data['nama_cabang'];
+                                            $id_cabang = $data['id_cabang'];
+                                        ?>
+                                            <tr>
+                                                <td><?= $id_pegawai; ?></td>
+                                                <td><?= $nama_pegawai; ?></td>
+                                                <td><?= $alamat_pgw; ?></td>
+                                                <td><?= $nama_cabang; ?></td>
+                                                <td>
+                                                    <button style="margin: 2px;" type="button" class="btn btn-warning" data-toggle="modal" data-target="#updatepegawai<?= $id_pegawai; ?>">Update</button>
+                                                    <button style="margin: 2px;" type="button" class="btn btn-danger" data-toggle="modal" data-target="#deletepegawai<?= $id_pegawai; ?>">Delete</button>
+                                                </td>
+                                            </tr>
+
+                                            <!-- modal update pegawai -->
+                                            <div class="modal fade" id="updatepegawai<?= $id_pegawai; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="exampleModalLabel">Update Data Karyawan</h5>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </div>
+                                                        <form method="POST">
+                                                            <div class="modal-body">
+                                                                <input type="text" name="id_pegawai" value="<?= $id_pegawai; ?>" class="form-control">
+                                                                <br />
+                                                                <input type="text" name="nama_pegawai" value="<?= $nama_pegawai; ?>" class="form-control">
+                                                                <br />
+                                                                <input type="text" name="alamat_pgw" value="<?= $alamat_pgw; ?>" class="form-control">
+                                                                <br />
+                                                                <select name="id_cabang" class="form-control">
+                                                                    <option selected value="<?= $id_cabang; ?>"></option>
+                                                                    <?php
+                                                                    $tampilancabang = mysqli_query($conn, "select * from cabang");
+                                                                    while ($fetcharray = mysqli_fetch_array($tampilancabang)) {
+                                                                        $nama_cabang = $fetcharray['nama_cabang'];
+                                                                        $id_cabang = $fetcharray['id_cabang'];
+                                                                    ?>
+                                                                        <option value="<?= $id_cabang; ?>"><?= $nama_cabang; ?></option>
+
+                                                                    <?php
+                                                                    }
+                                                                    ?>
+                                                                </select>
+                                                                <br />
+                                                                <input type="hidden" name="id_pegawai" value="<?= $id_pegawai; ?>">
+                                                                <br />
+                                                                <button type="submit" name="updatepegawai" class="btn btn-warning">Update</button>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- modal update pegawai -->
+
+                                            <!-- delete modal pegawai -->
+                                            <div class="modal fade" id="deletepegawai<?= $id_pegawai; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="exampleModalLabel">Hapus KB</h5>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </div>
+                                                        <form method="POST">
+                                                            <div class="modal-body">
+                                                                Apakah anda ingin menghapus data ini?
+                                                                <input type="hidden" name="id_pegawai" value="<?= $id_pegawai ?>">
+                                                                <button type="submit" name="deletepegawai" class="btn btn-danger">Hapus</button>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- delete modal pegawai-->
+
+                                        <?php
+                                        };
+
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+            </footer>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/datatables-demo.js"></script>
+</body>
+
+<!-- Modal -->
+<div class="modal fade" id="pgwmodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Karyawan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form method="POST">
+                <div class="modal-body">
+                    <input type="text" name="id_pegawai" placeholder="id_pegawai" class="form-control">
+                    <br />
+                    <input type="text" name="nama_pegawai" placeholder="nama_pegawai" class="form-control">
+                    <br />
+                    <input type="text" name="alamat_pgw" placeholder="alamat_pgw" class="form-control">
+                    <br />
+                    <select name="id_cabang" class="form-control">
+                        <option selected value="<?= $id_cabang; ?>">pilih cabang</option>
+                        <?php
+                        $tampilancabang = mysqli_query($conn, "select * from cabang");
+                        while ($fetcharray = mysqli_fetch_array($tampilancabang)) {
+                            $nama_cabang = $fetcharray['nama_cabang'];
+                            $id_cabang = $fetcharray['id_cabang'];
+                        ?>
+                            <option value="<?= $id_cabang; ?>"><?= $nama_cabang; ?></option>
+
+                        <?php
+                        }
+                        ?>
+                    </select>
+                    <br />
+                    <button type="submit" name="addpegawai" class="btn btn-primary">Tambah</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+</html>
